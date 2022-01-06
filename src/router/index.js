d77d988ce14464b53ca719ddf2a042fa6b8ba836
Remon-Ansary <rasel15-10514@diu.edu.bd>
2022-01-06 13:40:14 +0600

    flight final

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Flight from '@/components/Flight'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Flight',
+      component: Flight
     }
   ]
 })
